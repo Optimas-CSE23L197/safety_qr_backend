7571c314f1d436379caa0d0e2b4e95496ed6438f
Animesh Karan <karananimesh144@gmail.com>
2026-03-04 19:47:45 +0530

    fix: update Redis configuration to use correct environment variables

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,8 +2,8 @@ import { Redis } from "@upstash/redis";
 import { env } from "./env.js";
 
 const redis = new Redis({
-  url: env.upstash_url,
-  token: env.upstash_token,
+  url: env.upstash_redis_rest_url,
+  token: env.upstash_redis_rest_token,
 });
 
 export default redis;
